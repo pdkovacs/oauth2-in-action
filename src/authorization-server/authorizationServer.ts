248d848928e5b37c15113f03e596ee0974c2cc74
Kovács, Péter <peter.dunay.kovacs@gmail.com>
2018-02-24 16:42:01 +0100

    Fake a more realistic redirect URL setting for login at client

--- src/authorization-server/authorizationServer.ts
+++ src/authorization-server/authorizationServer.ts
@@ -31,7 +31,7 @@ const authServer = {
     tokenEndpoint: "http://localhost:9001/token"
 };
 
-app.get("/", (req, res) => {
+app.get("/", (req: express.Request, res: express.Response) => {
     res.render("index", {clients: getClients(), authServer});
 });
 
@@ -39,6 +39,11 @@ app.get("/authorize", authorizationEndpoint);
 app.post("/approve", approveEndpoint);
 app.post("/token", tokenEndpoint);
 app.get("/publickey", publickeyEndpoint);
+app.get("/logout", (req: express.Request, res: express.Response) => {
+    logger.log("info", "Place holder /logout end-point");
+    logger.log("info", "Request to redirect to %s after logging user back is noted", req.query.service);
+    res.end();
+});
 
 // app.options("/*", (req, res, next) => {
 //     res.header("Access-Control-Allow-Origin", "*");
