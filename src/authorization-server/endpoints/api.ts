94cda61fdba2383990bf32b9ee3c5eb2a9fe9d13
Kovács, Péter <peter.dunay.kovacs@gmail.com>
2020-04-19 16:50:25 +0200

    typescript upgrade

---
+++ src/authorization-server/endpoints/api.ts
@@ -0,0 +1,40 @@
+import { Request, Response } from "express";
+
+const group1 = {
+    groupName: "group1",
+    id: 0,
+    members: [
+        {
+            id: 0,
+            userName: "user1"
+        }
+    ],
+    team: {
+        demoTeam: true,
+        id: 0,
+        liveChatOn: true,
+        subdomain: "string"
+    }
+};
+
+const group2 = {
+    groupName: "group2",
+    id: 0,
+    members: [
+        {
+            id: 0,
+            userName: "user2"
+        }
+    ],
+    team: {
+        demoTeam: true,
+        id: 0,
+        liveChatOn: true,
+        subdomain: "string"
+    }
+};
+
+export const getGroups = (req: Request, res: Response) => {
+    console.log("getGroups headers", req.headers);
+    res.json([group1, group2]);
+};
